92e7f923de05fd1f5c2cfed3470c497ec521150a
James McClain <jmcclain@uni.edu>
2021-03-24 13:35:12 -0500

    Changes to setup script.
    * Remove 'deactivate' calls in setup script
      - Allows ". ./setup.sh setup" to both setup the virtualenv and
        activate it in the current shell.
    
    * Move virtualenv creation into the 'activate_env' function.
      - Not exactly necessary, but I felt it made since.

--- setup.sh
+++ setup.sh
@@ -4,23 +4,27 @@ VENV_NAME=virtualenv
 BIN="$VENV_NAME/bin"
 
 activate_env() {
+  if ! [ -d "$VENV_NAME" ]; then
+    python3 -m venv $VENV_NAME
+  fi
+
   if [ -d "$VENV_NAME/Scripts" ]; then
     BIN="$VENV_NAME/Scripts"
   fi
 
-  . ./$BIN/activate
+  if [ -f ./$BIN/activate ]; then
+    . ./$BIN/activate
+  else
+    exit 1
+  fi
 }
 
 setup() {
-  python3 -m venv $VENV_NAME
-
   activate_env
 
   pip install -r requirements.txt
   pip install -r gpt-2/requirements.txt
   pip install tensorflow==2.4.1
-
-  deactivate
 }
 
 clean() {
@@ -32,11 +36,11 @@ setup_development_stuff() {
   activate_env
 
   pip install jedi yapf pylint neovim
-
-  deactivate
 }
 
-if [ "$1"x = "setup"x ]; then
+if [ "$1"x = "activate_env"x ]; then
+  activate_env
+elif [ "$1"x = "setup"x ]; then
   setup
 elif [ "$1"x = "clean"x ]; then
   clean

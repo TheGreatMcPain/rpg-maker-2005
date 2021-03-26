7359d27b65c33916fa640134c25ff2a0a14a5716
James McClain <jmcclain@uni.edu>
2021-03-26 00:29:57 -0500

    setup.sh: Add "setup_no_tensorflow" function and fix "activate_env"
    "setup_no_tensorflow" will run the setup, but will allow access to
    system site-packages which will allow system tensorflow to work.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
 VENV_NAME=virtualenv
-BIN="$VENV_NAME/bin"
+# Not exactly portable POSIX, but it should work on
+# most systems
+SCRIPT_LOCATION=$(dirname $0)
+BIN="$SCRIPT_LOCATION/$VENV_NAME/bin"
 
 activate_env() {
-  if ! [ -d "$VENV_NAME" ]; then
-    python3 -m venv $VENV_NAME
+  if ! [ -d "$SCRIPT_LOCATION/$VENV_NAME" ]; then
+    if [ "$1"x = "--system-site-packages"x ]; then
+      python3 -m venv "$SCRIPT_LOCATION/$VENV_NAME" --system-site-packages
+    else
+      python3 -m venv "$SCRIPT_LOCATION/$VENV_NAME"
+    fi
   fi
 
-  if [ -d "$VENV_NAME/Scripts" ]; then
-    BIN="$VENV_NAME/Scripts"
+  if [ -d "$SCRIPT_LOCATION/$VENV_NAME/Scripts" ]; then
+    BIN="$SCRIPT_LOCATION/$VENV_NAME/Scripts"
   fi
 
   if [ -f ./$BIN/activate ]; then
@@ -17,6 +24,13 @@ activate_env() {
   else
     exit 1
   fi
+}
+
+setup_no_tensorflow() {
+  activate_env --system-site-packages
+
+  pip install -r requirements.txt
+  pip install -r gpt-2/requirements.txt
 }
 
 setup() {
@@ -29,7 +43,7 @@ setup() {
 
 clean() {
   rm -rv __pycache__
-  rm -rv $VENV_NAME
+  rm -rv "$SCRIPT_LOCATION/$VENV_NAME"
 }
 
 setup_development_stuff() {
@@ -42,6 +56,8 @@ if [ "$1"x = "activate_env"x ]; then
   activate_env
 elif [ "$1"x = "setup"x ]; then
   setup
+elif [ "$1"x = "setup_no_tensorflow"x ]; then
+  setup_no_tensorflow
 elif [ "$1"x = "clean"x ]; then
   clean
 elif [ "$1"x = "setup_development_stuff"x ]; then
